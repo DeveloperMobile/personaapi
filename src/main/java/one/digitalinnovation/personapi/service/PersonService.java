42a27ce6cae98b2fb7b370076280ac47fef24466
DeveloperMobile <tiagopereiravieira@gmail.com>
2022-04-27 16:35:55 -0300

    Developed person operation create

---
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.personapi.service;
+
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+    @Autowired
+    public PersonRepository rep;
+
+    public MessageResponseDTO createPerson(Person person) {
+        Person savedPerson = rep.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
